d75c5725f37e0acfc504ab0f919c54c0a4501865
EsranurGalipKayahan <61011249+EsranurGalipKayahan@users.noreply.github.com>
2022-07-30 00:48:48 +0200

    useFetch hook was written

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ProductController } from './components/ProductController'
+
+function App() {
+  return (
+    <div className="App">
+      <ProductController />
+    </div>
+  )
+}
+
+export default App
---
+++ src/data/constants.ts
@@ -0,0 +1,2 @@
+export const EYELINER_URL =
+  'https://makeup-api.herokuapp.com/api/v1/products.json?product_category=liquid&product_type=eyeliner'
